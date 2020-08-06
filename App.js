8d4d880bbe0a1b1a2b08ba543272c9c7e549d17f
Dave Genest <drwgenest@gmail.com>
2020-08-06 08:28:12 -0700

    Configure amplify

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva'
+import Amplify from '@aws-amplify/core'
+import aws_exports from './src/aws-exports'
+import { ApplicationProvider } from '@ui-kitten/components'
 import AuthContainer from './src/components/auth/AuthContainer'
-import { WGCProvider } from './src/store/context';
+import { WGCProvider } from './src/store/context'
+Amplify.configure(aws_exports)
 
 export default function App() {
   return (

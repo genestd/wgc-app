f0b2451f79ffcb7bbeab6b27bf3a368da9ebde78
Dave Genest <drwgenest@gmail.com>
2020-08-06 12:51:31 -0700

    Refactors AuthContainer Adds common functionality Renders page based on auth state Adds keyboardavoidingview

--- src/components/auth/AuthContainer.js
+++ src/components/auth/AuthContainer.js
@@ -1,37 +1,65 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import React, { useContext } from 'react'
+import { View, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
 import { LinearGradient } from 'expo-linear-gradient'
 import Logo from '../shared/Logo'
 import Login from './Login'
 import Register from './Register'
 import ConfirmRegistration from './ConfirmRegistration'
 import ResetPassword from './ResetPassword'
+import { WGCContext } from '../../store/context'
+import { CHANGE_AUTH_PAGE } from '../../store/actionTypes'
 
 const AuthContainer = () => {
+    const { state, dispatch } = useContext(WGCContext)
+    function changeAuthPage (newPage) {
+        dispatch({ type: CHANGE_AUTH_PAGE, payload: newPage })
+    }
+
+    function renderComponent (pageName) {
+        switch (pageName) {
+            case 'login':
+                return <Login
+                    onChangeAuthPage={changeAuthPage}
+                    loading={state.pendingActions.includes('LOGIN')}
+                />
+            case 'register':
+                return <Register onChangeAuthPage={changeAuthPage} />
+            case 'confirmRegistration':
+                return <ConfirmRegistration />
+            case 'resetPassword':
+                return <ResetPassword />
+            default:
+                return <Login />
+        }
+    }
     return (
-        <View style={styles.loginContainer}>
-            <LinearGradient
-                style={styles.loginGradient}
-                colors={['rgba(85, 88, 90, 1)', 'rgba(60, 63, 65, 1)', 'rgba(35, 38, 40, 1)']}
-            >
-                <Logo />
-                <ResetPassword />
-            </LinearGradient>
-        </View>
+        <KeyboardAvoidingView behavior='padding' style={styles.authContainer}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <LinearGradient
+                    style={styles.authGradient}
+                    colors={['rgba(85, 88, 90, 1)', 'rgba(60, 63, 65, 1)', 'rgba(35, 38, 40, 1)']}
+                >
+                    <Logo />
+                    {renderComponent(state.authPage)}
+                    <View style={{ flex: 1 }} />
+                </LinearGradient>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     )
 }
 
 const styles = StyleSheet.create({
-    loginContainer: {
+    authContainer: {
         flex: 1,
         width: '100%',
     },
-    loginGradient: {
-        paddingTop: 15,
+    authGradient: {
+        paddingTop: Constants.statusBarHeight,
         flex: 1,
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'flex-end',
         alignItems: 'center',
         width: '100%'
     },

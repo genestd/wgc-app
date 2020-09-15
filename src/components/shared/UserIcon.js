86562cd0890abebc50afc1a9267e7e4024c1afa1
Dave Genest <drwgenest@gmail.com>
2020-09-14 22:28:53 -0700

    Updates icon to handle avatar

--- src/components/shared/UserIcon.js
+++ src/components/shared/UserIcon.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import { Text, Layout, useTheme } from '@ui-kitten/components'
+import { S3Image } from 'aws-amplify-react-native'
 import { StyleSheet } from 'react-native'
 
-const UserIcon = ({ avatar, username, offset=0, overlap=true }) => {
+const UserIcon = ({ avatar, username, offset=0, overlap=true, size='normal' }) => {
     const theme = useTheme()
     const styles = StyleSheet.create({
-        initCapAvatar: {
+        initCapTextAvatar: {
             color: theme['color-info-500'],
+            fontSize: size === 'normal' ? 14 : 50,
+            fontFamily: size === 'normal' ? 'Spartan_500Medium' : 'Spartan_700Bold'
         },
-        initCapContainerOverlap: {
+        avatarContainer: {
             borderWidth: 2,
             borderColor: theme['color-primary-500'],
             backgroundColor: theme['color-primary-200'],
             borderRadius: 25,
-            width: 30,
-            height: 30,
-            paddingTop: 3,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        avatarContainerOverlap: {
+            borderWidth: 2,
+            borderColor: theme['color-primary-500'],
+            backgroundColor: theme['color-primary-200'],
+            borderRadius: 25,
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
@@ -28,19 +37,47 @@ const UserIcon = ({ avatar, username, offset=0, overlap=true }) => {
             borderColor: theme['color-primary-500'],
             backgroundColor: theme['color-primary-200'],
             borderRadius: 25,
-            width: 30,
-            height: 30,
-            paddingTop: 3,
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
+            paddingTop: size === 'normal' ? 3 : 15,
+        },
+        initCapContainerOverlap: {
+            borderWidth: 2,
+            borderColor: theme['color-primary-500'],
+            backgroundColor: theme['color-primary-200'],
+            borderRadius: 25,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingTop: size === 'normal' ? 3 : 15,
+            position: 'absolute',
+            transform: [{translateX: offset}],
+            zIndex: offset,
+        },
+        normal: {
+            height: 30,
+            width: 30,
+            borderRadius: 25
+        },
+        large: {
+            height: 120,
+            width: 120,
+            borderRadius: 120,
         }
 
     })
+    
     return (
-        <Layout style={overlap ? { ...styles.initCapContainerOverlap} : {...styles.initCapContainer}}>
-            <Text style={styles.initCapAvatar}>{username[0].toUpperCase()}</Text>
-        </Layout>
+        avatar ? (
+            <Layout style={overlap ? {...styles.avatarContainerOverlap, ...styles[size]} : {...styles.avatarContainer, ...styles[size]}}>
+                <S3Image imgKey={avatar} style={styles[size]} />
+            </Layout>
+        ) : (
+            <Layout style={overlap ? {...styles.initCapContainerOverlap, ...styles[size]} : {...styles.initCapContainer, ...styles[size]}}>
+                <Text style={styles.initCapTextAvatar}>{username[0].toUpperCase()}</Text>
+            </Layout>
+        )
     )
 }
 

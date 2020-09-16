138f318cb229a87817fa5a75fc93110ca4249d33
Dave Genest <drwgenest@gmail.com>
2020-09-15 23:13:48 -0700

    Improves editing experience Adds bio to settings

--- src/components/screens/SettingsScreen.js
+++ src/components/screens/SettingsScreen.js
@@ -2,19 +2,41 @@ import React, { useContext, useState } from 'react'
 import { TouchableOpacity, StyleSheet, Platform } from 'react-native'
 import * as Permissions from 'expo-permissions'
 import * as ImagePicker from 'expo-image-picker'
-import { Layout, Text, Icon, Input } from '@ui-kitten/components'
-import API, { graphqlOperation } from '@aws-amplify/api'
+import Constants from 'expo-constants'
+import { Layout, Text, Input } from '@ui-kitten/components'
 import ScreenHeader from '../shared/ScreenHeader'
 import UserIcon from '../shared/UserIcon'
-import { updateUser } from '../../graphql/mutations'
 import { WGCGlobalContext } from '../../globalStore/context'
 import { UPDATE_USER } from '../../globalStore/globalActionTypes'
 import { updateUserData } from '../../globalStore/globalActions'
 
-const SettingsScreen = (props) => {
+const SettingsScreen = () => {
     const { globalState: { user }, globalDispatch} = useContext(WGCGlobalContext)
-    const [editingName, setEditingName] = useState(false)
+    const [editing, setEditing] = useState(false)
     const [newName, setNewName] = useState(user.screenName || user.id)
+    const [newBio, setNewBio] = useState(user.bio)
+    const EditButton = () => (
+        <TouchableOpacity
+            onPress={() => setEditing(true)}
+            style={{ paddingTop: Constants.statusBarHeight, backgroundColor: 'transparent', height: 100, position: 'absolute', right: 15, bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+        >
+            <Text>Edit</Text>
+        </TouchableOpacity>
+    ) 
+    const SaveButton = () => (
+        <TouchableOpacity
+            onPress={saveSettings}
+            style={{ paddingTop: Constants.statusBarHeight, backgroundColor: 'transparent', height: 100, position: 'absolute', right: 15, bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+        >
+            <Text>Save</Text>
+        </TouchableOpacity>
+    )
+    const saveSettings = () => {
+        setEditing(false)
+        if (newName !== user.screenName || newBio !== user.bio) {
+            globalDispatch({ type: UPDATE_USER, user: { screenName: newName, bio: newBio }})
+        }
+    }
     const showImagePicker = async () => {
         try {
             const permission = await Permissions.askAsync(Permissions.CAMERA_ROLL)
@@ -30,7 +52,6 @@ const SettingsScreen = (props) => {
                         const image = await fetch(Platform.OS === 'android' ? pickerResult.uri : pickerResult.uri.replace('file://', ''))
                         const blob = await image.blob()
                         updateUserData(user.id, { avatar:  blob}, globalDispatch)
-                        globalDispatch({ type: UPDATE_USER, user: { avatar: pickerResult.uri }})
                     }
                 } catch (imageError) {
                     console.log(imageError)
@@ -40,56 +61,82 @@ const SettingsScreen = (props) => {
             console.log(err)
         }
     }
-    const saveNewName = async () => {
-        try {
-            setEditingName(false)
-            console.log(`setting ${user.id} screen name to ${newName}`)
-            await API.graphql(graphqlOperation(updateUser, {
-                input: {
-                    id: user.id,
-                    screenName: newName,
-                }
-            }))
-            globalDispatch({ type: UPDATE_USER, user: { screenName: newName }})
-        } catch (error) {
-            console.log(error)
-        }
-    }
     return (
         <Layout style={styles.screen}>
-            <ScreenHeader iconName='options-2-outline' title='Settings' />
-            <Layout style={{...styles.row, ...styles.avatar}}>
-                <TouchableOpacity onPress={showImagePicker}>
-                    <UserIcon
-                        avatar={user.avatar}
-                        username={user.id}
-                        overlap={false}
-                        size='large'
-                    />
-                </TouchableOpacity>
-            </Layout>
-            <Layout style={styles.body}>
-                <Text category='h4'>{`@${user.id}`}</Text>
-                    {!editingName 
-                        ? (
-                            <Layout style={styles.row}>
-                                <Text category='s1'>{user.screenName || user.id}</Text>
-                                <Icon name='edit-outline' fill='black' style={{ width: 25, height: 25}} onPress={() => setEditingName(true)} />
-                            </Layout>
-                        ) : (
-                            <Input
-                                autoFocus
-                                onBlur={saveNewName}
-                                maxLength={30}
-                                value={newName}
-                                onChangeText={setNewName}
+            <ScreenHeader
+                iconName='options-2-outline'
+                title='Settings'
+                headerRight={editing ? SaveButton : EditButton}
+            />
+            {editing ? (
+                <>
+                    <Layout style={{...styles.row, ...styles.avatar}}>
+                        <TouchableOpacity onPress={showImagePicker}>
+                            <UserIcon
+                                avatar={user.avatar}
+                                username={user.id}
+                                overlap={false}
+                                size='large'
                             />
-                        )
-                    }   
-            </Layout>
+                        </TouchableOpacity>
+                    </Layout>
+                    <Layout style={styles.body}>
+                        <Input
+                            autoFocus
+                            maxLength={30}
+                            value={newName}
+                            onChangeText={setNewName}
+                        />
+                        <Layout style={styles.row}>
+                            <Text category='h6'>{`@${user.id}`}</Text>
+                        </Layout>
+                        <Layout style={styles.bioContainer}>
+                            <Text category='h6' style={styles.title}>
+                                About
+                            </Text>
+                            <Input
+                                multiline
+                                value={newBio}
+                                onChangeText={setNewBio}
+                            />
+                        </Layout>
+                    </Layout>
+                </>
+            ) :
+                <DisplaySettings user={user} />
+            }
         </Layout>
     )
 }
+
+const DisplaySettings = ({ user }) => (
+    <>
+        <Layout style={{...styles.row, ...styles.avatar}}>
+            <UserIcon
+                avatar={user.avatar}
+                username={user.id}
+                overlap={false}
+                size='large'
+            />
+        </Layout>
+        <Layout style={styles.body}>
+            <Text category='h5'>
+                {user.screenName || user.id}
+            </Text>
+            <Layout style={styles.row}>
+                <Text category='h6'>{`@${user.id}`}</Text>
+            </Layout>
+            <Layout style={styles.bioContainer}>
+                <Text category='h6' style={styles.title}>
+                    About
+                </Text>
+                <Text style={styles.bio}>
+                    {user.bio}
+                </Text>
+            </Layout>
+        </Layout>
+    </>
+)
 
 const styles = StyleSheet.create({
     screen: {
@@ -104,11 +151,25 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        paddingTop: 10
     },
     avatar: {
         justifyContent: 'center',
         height: 150 ,
+    },
+    bioContainer: {
+        paddingTop: 30,
+    },
+    title: {
+        fontFamily: 'Spartan_700Bold'
+    },
+    bio: {
+        paddingTop: 15
+    },
+    bioEdit: {
+        marginLeft: 15,
+        marginRight: 15
     }
 })
 
